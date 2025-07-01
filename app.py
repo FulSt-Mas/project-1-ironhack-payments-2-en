f0c0745a3bb65fa46d1813f7d2c9df94c1fee437
Massih Chopan <194472108+FulSt-Mas@users.noreply.github.com>
2025-07-01 20:58:27 +0200

    no fnct in app.py

--- app.py
+++ app.py
@@ -4,26 +4,27 @@ from components.layout import LayoutCreator
 from components.callbacks import CallbackManager
 from utils.dataloader import KPILoader
 
-def initialize_app():
+
+# def initialize_app():
     # Load the data
-    kpi_loader = KPILoader('../project_dataset/extract - fees - data analyst.csv',
+kpi_loader = KPILoader('../project_dataset/extract - fees - data analyst.csv',
                            '../project_dataset/extract - cash request - data analyst.csv')
-    df1, df2 = kpi_loader.load_data()
+df1, df2 = kpi_loader.load_data()
 
     # Create the app
-    app = dash.Dash(__name__)
-    app.title = "IronHack Payments KPI Dashboard"
+app = dash.Dash(__name__)
+app.title = "IronHack Payments KPI Dashboard"
 
     # Set layout
-    layout_creator = LayoutCreator()
-    app.layout = layout_creator.create_layout()
+layout_creator = LayoutCreator()
+app.layout = layout_creator.create_layout()
 
     # Register callbacks
-    callback_manager = CallbackManager()
-    callback_manager.register_callbacks(app, df1, df2)
+callback_manager = CallbackManager()
+callback_manager.register_callbacks(app, df1, df2)
 
-    return app
+    # return app
 
 if __name__ == '__main__':
-    app = initialize_app()
+    # app = initialize_app()
     app.run(debug=True)
